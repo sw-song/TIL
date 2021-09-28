316da71a094b646e117c90caf7f30c65253b1de4
sw-song <sw930601@gmail.com>
2021-09-28 19:11:50 +0900

    실행파일 추가

--- C/address_2d_array.c
+++ C/address_2d_array.c
@@ -4,6 +4,11 @@ int main(void){
     int arr[2][3];
 
     printf("%p\n", arr);
+    printf("%p\n", &arr[0]);
     printf("%p\n", arr[0]);
-    printf("%sp\n", &arr[0][0]);
+    printf("%p\n", &arr[0][0]);
+
+    // 위 출력 값은 모두 동일하다
+    // arr은 arr[0]의 주소, arr[0]은 arr[0][0]의 주소
+    return 0;
 }
